2cf6e960eff032ecde7b6cc083fd35dc32312157
Emerson Almeida <emerson.almeida@adviseu.com.br>
2023-03-08 09:05:46 -0300

    aula - 28 - Criando a tela de pedidos

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>ORDER</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashborad />
   </div>
 </template>
+
+<script>
+import Dashboard from "../components/Dashboard.vue";
+export default {
+  name: "OrderView",
+
+  components: {
+    Dashboard
+  }
+};
+</script>
